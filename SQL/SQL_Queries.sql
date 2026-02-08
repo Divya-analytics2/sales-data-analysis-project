439a93f5f3ebd3e71613c88b17899b0414c2ecaa
Divya-analytics2 <divyanalli456@gmail.com>
2026-02-08 07:05:07 +0530

    Update SQL_Queries.sql

--- SQL/SQL_Queries.sql
+++ SQL/SQL_Queries.sql
@@ -1,1 +1,11 @@
 SQL queries for sales data analysis
+-- Monthly Sales Trend
+SELECT MONTH_ID, SUM(SALES) AS monthly_sales
+FROM sales
+GROUP BY MONTH_ID
+ORDER BY MONTH_ID;
+
+-- Sales by Product Line
+SELECT PRODUCTLINE, SUM(SALES) AS total_sales
+FROM sales
+GROUP BY PRODUCTLINE;
